dc6f6bcf70c7327b29c246eee203c7b797995589
Derflex-primo <dryldeogracias@gmail.com>
2025-01-13 23:41:03 +0800

    draft: express server

---
+++ src/src.ts
@@ -0,0 +1,24 @@
+import express, { Request, Response } from "express";
+import swaggerUi from 'swagger-ui-express';
+import yaml from "yaml";
+
+import { readFileSync } from "fs";
+
+
+const app = express();
+const port = 3000;
+
+// Middleware
+app.use(express.json());
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(yaml.parse(readFileSync('./src/swagger.yaml', 'utf8'))))
+
+// Routes
+app.get("/", (req: Request, res: Response) => {
+    res.send("Hello, TypeScript Express!");
+});
+
+
+// Start server
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
