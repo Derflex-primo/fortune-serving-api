b88057c08988a124b4db2fdd4772bf7c435a1f87
Derflex-primo <dryldeogracias@gmail.com>
2025-02-05 01:33:08 +0800

    wip: draft auth

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,19 @@
 require('dotenv').config({ path: './src/.env' });
+
 import express, { Request, Response } from "express";
 import swaggerUi from 'swagger-ui-express';
 import yaml from "yaml";
 import router from "./routes"
+import config from "./auth/config";
+
 import { readFileSync } from "fs";
+import { auth } from "express-oauth2-jwt-bearer";
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+app.use(auth(config));
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(yaml.parse(readFileSync('./src/swagger/output/bundled.yaml', 'utf8'))))
 app.use('/apiv1', router);
 
---
+++ src/auth/config.ts
@@ -0,0 +1,7 @@
+const config = {
+    audience: 'https://fortuneserving-api.com',
+    issuerBaseURL: 'https://dev-m8h20qvrxlwgronx.us.auth0.com/',
+    tokenSigningAlg: 'RS256'
+}
+
+export default config;
